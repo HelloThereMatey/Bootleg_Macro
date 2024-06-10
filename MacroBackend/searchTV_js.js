3db0fcf5ceba3a1eab8da20e837ef3e6acdfbc42
Bootleg Macro Pleb <purpyfurcat@proton.me>
2024-06-10 14:16:08 +1000

    Making progress cunts..

--- MacroBackend/searchTV_js.js
+++ MacroBackend/searchTV_js.js
@@ -1,18 +1,17 @@
-const path = require('path');
+// const path = require('path');
 
-// Get the CONDA_PREFIX environment variable
-const condaPrefix = process.env.CONDA_PREFIX;
-console.log(condaPrefix);
+// // Get the CONDA_PREFIX environment variable
+// const condaPrefix = process.env.CONDA_PREFIX;
+// console.log(condaPrefix);
 
-// If CONDA_PREFIX is set, construct the path to the package
-if (condaPrefix) {
-    const packagePath = path.join(condaPrefix, 'lib', 'node_modules', 'lib', 'node_modules', '@mathieuc', 'tradingview');
-    const TradingView = require(packagePath);
-    // Use the TradingView module as needed
-} else {
-    console.error('CONDA_PREFIX is not set. Please check your environment configuration.');
-}
-
+// // If CONDA_PREFIX is set, construct the path to the package
+// if (condaPrefix) {
+//     const packagePath = path.join(condaPrefix, 'lib', 'node_modules', 'lib', 'node_modules', '@mathieuc', 'tradingview');
+//     const TradingView = require(packagePath);
+//     // Use the TradingView module as needed
+// } else {
+//     console.error('CONDA_PREFIX is not set. Please check your environment configuration.');
+// }
 
 ///////////////////// Start script below ???????????????????????
 const fs = require('fs');
@@ -27,10 +26,10 @@ process.stdin.on('data', chunk => {
 console.log(data);
 process.stdin.on('end', () => {
     // Parse the JSON data
-    const obj = JSON.parse(data);
+    const searchstr = JSON.parse(data);
 
     // Suppose you're using a library to process this data
-    const processedData = TradingView.searchMarket(search = obj).then((rs) => {
+    const processedData = TradingView.searchMarket(search = searchstr).then((rs) => {
         const limitedResults = rs.slice(0, 30); // Adjust the number as needed
         console.log(limitedResults);
       });
